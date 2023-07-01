fe91d5af822dc2a0f91d379d113488f67749378a
Tuyana Chirninova <hikkonki@gmail.com>
2023-07-01 19:52:01 +0300

    add changes to channel

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,9 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+api_key: str = os.getenv('YOUTUBE_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -5,8 +11,9 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        dict_to_print = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
