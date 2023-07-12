42be0952401d21a293d8218443dd52e37281eabe
Tuyana Chirninova <hikkonki@gmail.com>
2023-07-12 03:22:55 +0300

    some comments added - tested PLVideo attributes

--- homework-4/main.py
+++ homework-4/main.py
@@ -6,3 +6,10 @@ if __name__ == '__main__':
     video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
     assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
     assert str(video2) == 'MoscowPython Meetup 78 - вступление'
+
+    # video1.print_vid_data()
+    # video2.print_vid_data()
+    # assert video2.title == "MoscowPython Meetup 78 - вступление"
+    # assert video2.url == 'https://wwww.youtube.com/4fObz_qw9u4'
+    # assert video2.view_count == '606'
+    # assert video2.like_count == '9'
