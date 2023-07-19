9bc0e1038dc7549f850b1c14822552b3f131a946
Tuyana Chirninova <hikkonki@gmail.com>
2023-07-19 19:48:17 +0300

    try/except block added

--- src/video.py
+++ src/video.py
@@ -3,19 +3,25 @@ from src.channel import Channel
 
 
 class Video:
-
     youtube = Channel.get_service()
     youtube_url = 'https://wwww.youtube.com/'
 
     def __init__(self, video_id: str):
-        self.video_id = video_id
-        self.video_data = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                     id=video_id
-                                                     ).execute()
-        self.title = self.video_data['items'][0]['snippet']['title']
-        self.url = self.youtube_url + self.video_data['items'][0]['id']
-        self.view_count = self.video_data['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_data['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self.video_data = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                         id=video_id
+                                                         ).execute()
+            self.title = self.video_data['items'][0]['snippet']['title']
+            self.url = self.youtube_url + self.video_data['items'][0]['id']
+            self.view_count = self.video_data['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_data['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return f'{self.title}'
